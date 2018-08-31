157654599a501fb4a467e909374f6a2cfd15e811
jorasmhj <jorasmhj@gmail.com>
2018-08-31 21:23:31 +0545

    admin products component change

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -3,24 +3,26 @@ import { ProductService } from '../../product.service';
 import { Product } from '../../models/product';
 
 @Component({
-  selector: 'app-admin-products',
-  templateUrl: './admin-products.component.html',
-  styleUrls: ['./admin-products.component.css']
+    selector: 'app-admin-products',
+    templateUrl: './admin-products.component.html',
+    styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
-  products: Product[];
+    products: Product[];
 
-  constructor(private productService: ProductService) {
-    this.productService.getAll().subscribe(p => {
-      p.forEach(p => {
-        let data = p['data'];
-        data['key'] =p['key'];
-        this.products.push
-      });
-    });
-  }
+    constructor(private productService: ProductService) {
+        this.productService.getAll().subscribe(p => {
+            let obj = [];
+            p.forEach(p => {
+                let data = p['data'];
+                data['key'] = p['key'];
+                obj.push(data);
+            });
+            this.products = obj;
+        });
+    }
 
-  ngOnInit() { }
+    ngOnInit() { }
 
 
 }
